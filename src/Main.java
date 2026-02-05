8915007d3aae47191ed874f723d5039190f7f838
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2026-02-05 12:09:28 +0100

    Clase Main Terminada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Double potencia= 50.0;
 
+        TorreMagica torre= new TorreMagica();
+        Mago mago = new MagoFuego("Gandalf", 200);
+
+        torre.getHabitantes().add(mago);
+
+
+        TorreMagica.realizarRitual(
+                torre.getHabitantes().get(0),
+                potencia
+        );
+
+        System.out.println("Energia del mago: "+mago.getEnergia());
+        System.out.println("Potencia: "+potencia);
     }
 }
