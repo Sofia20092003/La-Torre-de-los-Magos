8dd78a9d102b8d9aebcabde41fe57c22c3867355
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2026-02-05 11:51:48 +0100

    Clase Mago Terminada

--- src/Mago.java
+++ src/Mago.java
@@ -1,2 +1,21 @@
-public class Mago {
+import java.util.ArrayList;
+
+public abstract class Mago {
+    protected String nombre;
+    protected double energia;
+    protected ArrayList<String> hechizos;
+
+    protected static int totalMagiaMundial=1000;
+
+    public Mago(String nombre, double energia) {
+        this.nombre = nombre;
+        this.energia = energia;
+        this.hechizos= new ArrayList<>();
+    }
+
+    public double getEnergia() {
+        return energia;
+    }
+
+    public abstract void lanzarHechizos();
 }
