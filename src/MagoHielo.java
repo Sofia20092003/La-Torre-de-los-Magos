cdfd85180892e44acbde4254feacece39960cc3d
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2026-02-05 11:59:04 +0100

    Sub-Clase MagoHielo Terminada

--- src/MagoHielo.java
+++ src/MagoHielo.java
@@ -1,2 +1,12 @@
-public class MagoHielo {
+public class MagoHielo extends  Mago {
+
+    public MagoHielo(String nombre, double energia) {
+        super(nombre, energia);
+    }
+
+    @Override
+    public void lanzarHechizos() {
+        System.out.println(nombre+ " lanza un hechizo de hielo.");
+        energia -=15;
+    }
 }
