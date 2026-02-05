8e87e6a15852d0e71ee1ca415272631b97cc0f35
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2026-02-05 12:04:54 +0100

    Clase TorreMagica Terminada

--- src/TorreMagica.java
+++ src/TorreMagica.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class TorreMagica {
+
+    private ArrayList<Mago> habitantes;
+
+    public TorreMagica() {
+        habitantes = new ArrayList<>();
+    }
+
+    public ArrayList<Mago> getHabitantes() {
+        return habitantes;
+    }
+
+    public void entrenarMagos(){
+        for (Mago m: habitantes) {
+            m.lanzarHechizos();
+        }
+    }
+
+    public static void realizarRitual(Mago m, Double potencia){
+        m.energia -=30;
+        potencia +=100;
+
+        if (Mago.totalMagiaMundial>0){
+            Mago.totalMagiaMundial-=10;
+        }
+    }
 }
