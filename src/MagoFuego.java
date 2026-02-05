eba78477cffae4f84b075871d3f9e29a2499e399
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2026-02-05 11:55:13 +0100

    Sub-Clase MagoFuego Terminada

--- src/MagoFuego.java
+++ src/MagoFuego.java
@@ -1,2 +1,12 @@
-public class MagoFuego {
+public class MagoFuego extends Mago {
+
+    public MagoFuego(String nombre, double energia) {
+        super(nombre, energia);
+    }
+
+    @Override
+    public void lanzarHechizos() {
+        System.out.println(nombre+" lanza una bola de fuego.");
+        energia -=20;
+    }
 }
